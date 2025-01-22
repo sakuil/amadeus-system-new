2f7d936c7c01da69e7144d384e20a90f75e5bf66
huzw1995@gmail.com <huzw1995@gmail.com>
2025-01-22 23:58:04 +0800

    refactor: 移除无用的常量和映射

--- service/src/constants.ts
+++ service/src/constants.ts
@@ -1,15 +1,3 @@
-export const nameMapper = { steinsGate: '牧濑红莉栖(kurisu)，冈部伦太郎(okabe)，椎名真由理(mayuri)，比屋定真帆(maho)，阿万音铃羽(suzuha)，漆原琉华(Urushibara Ruka),桶子(daru)，雷斯吉宁(Leskinen)，桐生萌郁(Kiriyu Moeka),菲利斯(Faris NyanNyan)，天王寺裕吾(Mr.Braun)，椎名篝(Kagari)，绹(Tennouji nae)，阿万音由季(Yuki)，牧濑章一(Shouichi Makise)' }
-
-export const voiceToVoiceMapper: any = {
-  '4c0b21b2ddb247d8ba45a1c1e84afe64': '11',
-  'ca597fbfabaa496c8419e4e7c3ca95ef': '13',
-  '3e9fbf19ee114896ad0e8f38f6bd14e5': '12',
-  '6e51b1bfbd2f46689b37005ad50147ac': '10',
-  '354ac66e06614a0aa89626b0c8fc8797': '7',
-}
-
-export const filteredNames = ['蒋介石', '希特勒']
-
 export enum WebSocketMessageTypes {
   PING = 'ping',
   PONG = 'pong',
